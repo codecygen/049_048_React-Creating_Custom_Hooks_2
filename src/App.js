d41a6000ab9bf7d08ae4eeb539c451c79561f617
gingercook <vahitaras@gmail.com>
2021-12-24 23:54:35 -0500

    memoization useCallback method added to methods to prevent re-rendering

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+
+// React-Custom_Hooks_Function-Calling_Custom_Hook_From_Component
+import useHTTP from './hooks/use-http';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-custom-hooks-fetch-api-default-rtdb.firebaseio.com/tasks.json'
-      );
+  // Data specific login is kept on the main function
+  // It will be called by our custom hook as an argument, whenever it is needed
+  // React-Custom_Hooks_Function-Calling_Custom_Hook_From_Component
+  const transformTasks = useCallback(taskObj => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
     }
-    setIsLoading(false);
-  };
+
+    setTasks(loadedTasks);
+  // Here, we don't need to put any dependency.
+  // Because state updating function "setTasks" will
+  // never change. Other than that, nothing changes.
+  }, [])
+
+  // Here this function can just pass a single keyword for the object argument
+  // Other object keys are set to have default values and this is the reason they are 
+  // left out blank, it will get the default value that are
+  // assigned in "use-http.js"
+  // here "SendRequest" is a returned function whenever needed
+  // "transformTasks is a function hoisted from "use-http.js" to "App.js".
+  // Because original function resides on this level.
+  // React-Custom_Hooks_Function-Calling_Custom_Hook_From_Component
+  const fetchLink = 'https://react-custom-hooks-fetch-api-default-rtdb.firebaseio.com/tasks.json';
+  // Here, when we destructure with "sendRequest: fetchTasks", we assign an alias
+  // for the sendRequest key.
+  const { isLoading, error, sendRequest: fetchTasks } = useHTTP({ url: fetchLink },
+    transformTasks
+  );
 
   useEffect(() => {
+    // "fetchTasks" function literally resides in
+    // our custom hook and is returned by it
+    // whenever it is requested like in this case.
+    // This useEffect is used to have a GET request on page load.
+    // The function is then called from our custom hook, and it is
+    // returned from that hook to the "App" component.
     fetchTasks();
   }, []);
 
@@ -46,6 +57,12 @@ function App() {
   return (
     <React.Fragment>
       <NewTask onAddTask={taskAddHandler} />
+      {/* "fetchTasks" function literally resides in */}
+      {/* our custom hook and is returned by it */}
+      {/* whenever it is requested like in this case. */}
+      {/* This function is passed as props to "Tasks" component. */}
+      {/* Function is then hoisted to "App" component which is then*/}
+      {/* returned from our custom hook */}
       <Tasks
         items={tasks}
         loading={isLoading}
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,20 +1,21 @@
-import { useState } from "react";
+// React-Custom_Hooks_Function_File
+import { useState, useCallback } from "react";
 
 const useHTTP = (requestConfig, applyData) => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const sendRequest = async (taskText, postReq = true) => {
+    const sendRequest = useCallback(async () => {
         setIsLoading(true);
         setError(null);
 
         try {
             const response = await fetch(
                 requestConfig.url, {
-                    method: requestConfig.method,
-                    headers: requestConfig.headers,
-                    body: JSON.stringify(requestConfig.body)
-                }
+                method: requestConfig.method ? requestConfig.method : 'GET',
+                headers: requestConfig.headers ? requestConfig.headers : {},
+                body: requestConfig.body ? JSON.stringify(requestConfig.body) : null
+            }
             );
 
             if (!response.ok) {
@@ -23,10 +24,21 @@ const useHTTP = (requestConfig, applyData) => {
 
             const data = await response.json();
 
+            applyData(data);
+
         } catch (err) {
             setError(err.message || 'Something went wrong!');
         }
         setIsLoading(false);
+    }, [requestConfig, applyData]);
+
+    // This is a modern way of writing a Javascript code
+    // if key and value pairs are same, you can write the object
+    // like this.
+    return {
+        isLoading,
+        error,
+        sendRequest
     };
 };
 
